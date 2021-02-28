c559ffcea83b1b0a6d275b62aa58a14b77f515e3
AyoubMoujane <moujane.ayoub@hotmail.com>
2021-02-28 20:45:19 +0100

    Added route & remove unused files

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ app.get('/', function (req, res) {
   res.send("Hello World, here's my first CI/CD pipeline with jenkins, docker & aws");
 });
 
+app.get('/devops', function (req, res) {
+  res.send("worked fine in dev, ops problem now");
+});
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
 });
