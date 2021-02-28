2c674e669aa3bb0aefa67b7b43f265e9ba54312f
AyoubMoujane <moujane.ayoub@hotmail.com>
2021-02-28 15:57:39 +0100

    Added text

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send("Hello World, here's my first CI/CD pipeline with jenkins, docker & aws");
 });
 
 var server = app.listen(3000, function () {
